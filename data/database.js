29bc8eed4544d3d5f4968a145af96e164d8f76f0
Gautam Girdhar <149051230+GautamGirdhar@users.noreply.github.com>
2024-08-13 18:08:13 +0530

    Update database.js

--- data/database.js
+++ data/database.js
@@ -5,7 +5,7 @@ const MongoClient = mongodb.MongoClient;
 let database;
 
 async function connectToDatabase() {
-    const client = await MongoClient.connect('mongodb+srv://girdhargautam6:G%40ut%40m%4010122004@cluster0.7kyh0h0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    const client = await MongoClient.connect('');
     database = client.db('online-shop');
 }
 
